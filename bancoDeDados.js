1dab9d732336207f0a4e10e89deeca1382e26f59
Alanins <lilianemmoreira@gmail.com>
2024-10-08 16:54:50 -0300

    arrumando o bancoDeDados

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -5,7 +5,7 @@ async function conectaBancoDeDados() {
     try {
         console.log('Conexão com o banco de dados iniciou')
 
-        await mongoose.connect('process.env.MONGO_URL')
+        await mongoose.connect(process.env.MONGO_URL)
 
         console.log('Conexão com o banco de dados feita com sucesso!')
     } catch (erro) {
